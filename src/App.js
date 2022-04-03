b1ef32ca89340e1285fdff20a4701c4718c0946a
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-04-03 10:27:18 +0530

    feat:added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import {Routes,Route} from "react-router-dom";
 import {Navbar} from "./components/index";
+import {Home,Questions,Result,Rules} from "./pages/index";
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Routes>
-        {/* <Route path="/" element={<Home/>}/> */}
+        <Route path="/" element={<Home/>}/>
+        <Route path="/result" element={<Result/>}/>
+        <Route path="/rules" element={<Rules/>}/>
+        <Route path="/question" element={<Questions/>}/>
 
       </Routes>
     </div>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import { Link} from "react-router-dom";
+import "./Navbar.css";
+import { useState } from "react";
+
+export const Navbar = () => {
+  return(
+    <>Navbar</>
+  )
+};

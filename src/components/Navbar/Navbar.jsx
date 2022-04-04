37caba522760decef187005401c037246b325bb0
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-04-04 11:18:10 +0530

    addednavbar and homepage

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,43 @@ import "./Navbar.css";
 import { useState } from "react";
 
 export const Navbar = () => {
-  return(
-    <>Navbar</>
-  )
+  const [isMobile, setISMobile] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="left-side">
+        <img
+          className="logo-img"
+          src="../Assets/quizzylogo.png"
+          alt=""
+        />
+        <h6>QUIZZY</h6>
+      </div>
+
+      <ul
+        className={`list-style-none flex-row ${isMobile ? "nav-links-mobile" : "nav-links"}`}
+        onClick={() => setISMobile(false)}
+      >
+        <Link to="/" className="home">
+          <li>Home</li>
+        </Link>
+        <Link to="/dashboard" className="dashboard">
+          <li>Dashboard</li>
+        </Link>
+        <Link to="login" className="login">
+          <li>Login</li>
+        </Link>
+      </ul>
+      <button
+        className="mobile-menu-icon"
+        onClick={() => setISMobile(!isMobile)}
+      >
+        {isMobile ? (
+          <i class="fa fa-times" aria-hidden="true"></i>
+        ) : (
+          <i class="fa fa-bars" aria-hidden="true"></i>
+        )}
+      </button>
+    </nav>
+  );
 };
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,3 +1,24 @@
+import "./Home.css";
+import { Link } from "react-router-dom";
+import { category } from "../../data/data";
 export const Home = () => {
-    return <>Home</>;
+    return (
+      <section className="home__section">
+      <div className="category__container top-gutter-md">
+        {category.map((quiz) => (
+          <div className="category">
+            <Link to="/rules">
+              <div className="img-container">
+              <img src={quiz.image} alt="" className="responsive-img" />
+              </div>
+              
+              <div className="category__content">
+                <h3>{quiz.name}</h3>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </section>
+    );
   };
